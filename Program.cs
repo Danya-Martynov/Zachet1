3e8025003a586b0bf3d1cb832113261c6f721e5c
Danya-Martynov <66323130+Danya-Martynov@users.noreply.github.com>
2020-06-26 11:11:46 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[,] matr = new byte[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i + j == 3)
+                    {
+                        matr[i, j] = 1;
+                    }
+                    else
+                    {
+                        matr[i, j] = 0;
+                    }
+
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(matr[i, j].ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
